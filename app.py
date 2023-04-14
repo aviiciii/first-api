29dc874a755cd114654d7fa18af15082f825f5dd
aviiciii <laavesh1@gmail.com>
2023-04-14 10:47:55 +0530

    get hackathons route added

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/gethackthons", methods = ["GET"])
+def get_hackthons():
+    hackathon_list = ["Hackathon 1", "Hackathon 2", "Hackathon 3"]
+    return hackathon_list
+
+
+
+
 if __name__=="__main__":
 	app.run(debug = True)
